d98fde9396e3394f883ee02dd476d0431acb8976
Tristan <tristanelliottlorenz@gmail.com>
2025-10-02 15:46:57 -0400

    Updated main.py
    Now it is complete!

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import nltk
-from nltk import SentimentIntensityAnalyzer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 class Mood():
   def __init__(self, sentence):
     self.mood = ""
     self.sia = SentimentIntensityAnalyzer()
     self.sentence = sentence
+    self.score = {}
+    self.compound = 0
+  def mood(self):
+    self.score = self.sia.polarity_scores(self.sentence)
+    self.compound = self.scores['compound']
+    if self.compound == 0:
+      return "neu"
+    elif self.compound <= -0.05:
+      return "neg"
+    elif self.compound >= 0.05:
+      return "pos"
