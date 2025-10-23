fd28bf7d37f287ce8ac3b0a7b4e36fada9e3955f
Tristan <tristanelliottlorenz@gmail.com>
2025-10-23 15:34:00 -0400

    Correct mood classification and return statements
    Fix mood determination logic and return values.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+#pip3 install nltk
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#pip3 install colorama
+import colorama
+
+colorama.init()
 
 class Mood():
   def __init__(self, sentence):
@@ -12,8 +17,13 @@ class Mood():
     self.score = self.sia.polarity_scores(self.sentence)
     self.compound = self.scores['compound']
     if self.compound == 0:
-      return "neu"
+      self.mood = "Neutral"
+      return "neu", self.compound
     elif self.compound <= -0.05:
-      return "neg"
+      self.mood = "Negative"
+      return "neg", self.compund
     elif self.compound >= 0.05:
-      return "pos"
+      self.mood = "Positive"
+      return "pos", self.compund
+  def display_mood(self):
+    #print(f
